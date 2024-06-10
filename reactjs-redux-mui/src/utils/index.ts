846aa1ffd5befb0b0eaeb881a91d3b6d0ab50fb0
Jt_trịnh <89924574+iamnhatjt@users.noreply.github.com>
2024-06-10 23:14:06 +0700

    base: default until and enums

---
+++ reactjs-redux-mui/src/utils/index.ts
@@ -0,0 +1,13 @@
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: NodeJS.Timeout;
+  return (...args: Parameters<F>): void => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+};
