86a02e1919da5634f59e0f66ce50c08c92db1bbe
Jt_trịnh <89924574+iamnhatjt@users.noreply.github.com>
2024-06-11 09:22:23 +0700

    base: api client for base project

---
+++ reactjs-redux-mui/src/api/endpoint.ts
@@ -0,0 +1,4 @@
+export const Endpoint = {
+  // here we define a Endpoint for project
+  REFRESH_TOKEN: "",
+};
---
+++ reactjs-redux-mui/src/constant/index.ts
@@ -0,0 +1,6 @@
+export const ACCESS_TOKEN_STORAGE_KEY = "aT";
+export const REFRESH_TOKEN_STORAGE_KEY = "rT";
+
+export const API_TIMEOUT = 30_000; //s
+export const AUTH_API_URL = process.env.AUTH_API_URL as string;
+export const API_URL = process.env.API_URL as string;
